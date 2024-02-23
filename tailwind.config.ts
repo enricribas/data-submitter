ad5c2fc88bb9a74c7cf7b10f52628ad83bf7da75
Enric Ribas <enric.ribas@botco.ai>
2024-02-23 07:19:53 -0500

    Custom theme; little effort

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import { join } from "path";
 import type { Config } from "tailwindcss";
 import forms from "@tailwindcss/forms";
 import { skeleton } from "@skeletonlabs/tw-plugin";
+import { theme } from "./botco-theme";
 
 export default {
 	darkMode: "class",
@@ -16,12 +17,7 @@ export default {
 		forms,
 		skeleton({
 			themes: {
-				preset: [
-					{
-						name: "skeleton",
-						enhancements: true,
-					},
-				],
+				custom: [theme],
 			},
 		}),
 	],
