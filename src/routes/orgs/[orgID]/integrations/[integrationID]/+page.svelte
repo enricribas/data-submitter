9bd09f624f40439c347f334f8321e27643f100ee
Enric Ribas <enric.ribas@botco.ai>
2024-02-23 06:14:28 -0500

    Nicer cards around points

--- src/routes/orgs/[orgID]/integrations/[integrationID]/+page.svelte
+++ src/routes/orgs/[orgID]/integrations/[integrationID]/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { page } from "$app/stores";
 	import { pointsStore, subscribePointsStore } from "$lib/stores/pointsStore";
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
 
-	import PointWrapper from "./pointWrapper.svelte";
 	import CRM from "./crm.svelte";
 	import Email from "./email.svelte";
 
@@ -14,10 +14,18 @@
 	subscribePointsStore($page.params.integrationID);
 </script>
 
-<div class="m-3">
+<div class="m-3 ">
+	<Accordion spacing="space-y-2" >
 	{#each $pointsStore as point}
-		<PointWrapper title={point.type}>
-			<svelte:component this={options[point.type]} {point} />
-		</PointWrapper>
+		<div class="border rounded-lg border-slate-400">
+
+		<AccordionItem>
+			<svelte:fragment slot="summary"> <h2>{point.type}</h2> </svelte:fragment>
+			<svelte:fragment slot="content">
+				<svelte:component this={options[point.type]} {point} />
+			</svelte:fragment>
+		</AccordionItem>
+		</div>
 	{/each}
+	</Accordion>
 </div>
